db56d8e2785855d054aeeaf613a6767ff0ee444b
it27427 <istiak.tushar.qtec@gmail.com>
2024-10-18 01:22:51 +0600

    required package installed and imported in main

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -3,12 +3,12 @@ import notfound from '@/assets/images/errors/notfound.svg';
 const NotFound = () => {
   return (
     <>
-      <div className='flexcentercol'>
+      <div className='d-flex flex-column gap-4 align-items-center justify-content-center'>
         <figure className='notfoundthumb'>
           <img src={notfound} alt='not-found' />
         </figure>
 
-        <button type='button' className='btn-primary'>
+        <button type='button' className='btn btn-primary fw-bold py-3 px-4'>
           Back to Home
         </button>
       </div>
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 
+// BOOTSTRAP-CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min';
 
+// ANIMATE AND ICON
+import '@/assets/css/animate.css';
+import '@/assets/css/icofont.min.css';
+import '@/assets/css/magnific-popup.css';
+
+// SWIPER CSS
+import 'swiper/css';
+// CUSTOM CSS
 import '@/assets/scss/main.scss';
 
 import { BrowserRouter } from 'react-router-dom';
