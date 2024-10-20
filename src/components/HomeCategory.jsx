a3c2ca1d091d4264d156db2d951b603e005346c3
it27427 <istiak.tushar.qtec@gmail.com>
2024-10-20 20:28:22 +0600

    category-card

--- src/components/HomeCategory.jsx
+++ src/components/HomeCategory.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const subTitle = 'Choose Any Products';
 const title = 'Buy Everything with Us';
@@ -9,51 +10,70 @@ const categoryList = [
     id: 1,
     imgUrl: '@/assets/images/category/01.jpg',
     imgAlt: 'category rajibraj91 rajibraj',
-    iconName: 'icofont-brand-windows',
+    iconName: 'icofont-duotone icofont-brand-windows',
     title: 'DSLR Camera',
   },
   {
     id: 2,
     imgUrl: '@/assets/images/category/02.jpg',
     imgAlt: 'category rajibraj91 rajibraj',
-    iconName: 'icofont-brand-windows',
+    iconName: 'icofont-duotone icofont-brand-windows',
     title: 'Shoes',
   },
   {
     id: 3,
     imgUrl: '@/assets/images/category/03.jpg',
     imgAlt: 'category rajibraj91 rajibraj',
-    iconName: 'icofont-brand-windows',
+    iconName: 'icofont-duotone icofont-brand-windows',
     title: 'Photography',
   },
   {
     id: 4,
     imgUrl: '@/assets/images/category/04.jpg',
     imgAlt: 'category rajibraj91 rajibraj',
-    iconName: 'icofont-brand-windows',
+    iconName: 'icofont-duotone icofont-brand-windows',
     title: 'Formal Dress',
   },
   {
     id: 5,
     imgUrl: '@/assets/images/category/05.jpg',
     imgAlt: 'category rajibraj91 rajibraj',
-    iconName: 'icofont-brand-windows',
+    iconName: 'icofont-duotone icofont-brand-windows',
     title: 'Colorful Bags',
   },
   {
     id: 6,
     imgUrl: '@/assets/images/category/06.jpg',
     imgAlt: 'category rajibraj91 rajibraj',
-    iconName: 'icofont-brand-windows',
+    iconName: 'icofont-duotone icofont-brand-windows',
     title: 'Home Decor',
   },
 ];
 
 const HomeCategory = () => {
   return (
-    <div>
-      <h2>Home Category</h2>
-    </div>
+    <section className='category-section style-4 padding-tb'>
+      <div className='container'>
+        <hgroup className='section-header text-center'>
+          <h4 className='subtitle'>{subTitle}</h4>
+          <h2 className='title'>{title}</h2>
+        </hgroup>
+
+        <div className='section-wrapper'>
+          {categoryList.map((category) => (
+            <div key={category.id} className='col'>
+              <Link to='shop' className='category-item'>
+                <div className='category-inner'>
+                  <figure>
+                    <img src={category.imgUrl} alt={category.imgAlt} />
+                  </figure>
+                </div>
+              </Link>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
   );
 };
 
