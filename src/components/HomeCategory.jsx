69eb453f3e66e88618f907350104ccd857932bb6
it27427 <istiak.tushar.qtec@gmail.com>
2024-10-20 20:16:13 +0600

    category data added

--- src/components/HomeCategory.jsx
+++ src/components/HomeCategory.jsx
@@ -1,5 +1,54 @@
 import React from 'react';
 
+const subTitle = 'Choose Any Products';
+const title = 'Buy Everything with Us';
+const btnText = 'Get Started Now';
+
+const categoryList = [
+  {
+    id: 1,
+    imgUrl: '@/assets/images/category/01.jpg',
+    imgAlt: 'category rajibraj91 rajibraj',
+    iconName: 'icofont-brand-windows',
+    title: 'DSLR Camera',
+  },
+  {
+    id: 2,
+    imgUrl: '@/assets/images/category/02.jpg',
+    imgAlt: 'category rajibraj91 rajibraj',
+    iconName: 'icofont-brand-windows',
+    title: 'Shoes',
+  },
+  {
+    id: 3,
+    imgUrl: '@/assets/images/category/03.jpg',
+    imgAlt: 'category rajibraj91 rajibraj',
+    iconName: 'icofont-brand-windows',
+    title: 'Photography',
+  },
+  {
+    id: 4,
+    imgUrl: '@/assets/images/category/04.jpg',
+    imgAlt: 'category rajibraj91 rajibraj',
+    iconName: 'icofont-brand-windows',
+    title: 'Formal Dress',
+  },
+  {
+    id: 5,
+    imgUrl: '@/assets/images/category/05.jpg',
+    imgAlt: 'category rajibraj91 rajibraj',
+    iconName: 'icofont-brand-windows',
+    title: 'Colorful Bags',
+  },
+  {
+    id: 6,
+    imgUrl: '@/assets/images/category/06.jpg',
+    imgAlt: 'category rajibraj91 rajibraj',
+    iconName: 'icofont-brand-windows',
+    title: 'Home Decor',
+  },
+];
+
 const HomeCategory = () => {
   return (
     <div>
