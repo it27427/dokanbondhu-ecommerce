94c4608584953c7a31c9a2f60afba902b3c850d9
it27427 <istiak.tushar.qtec@gmail.com>
2024-10-20 15:12:33 +0600

    Hero Component created and it's title, desc and bannerList Items created

---
+++ src/components/Hero.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const title = <h2>Search Your One From Thousand of Products</h2>;
+const description = 'We have the largest collection of products.';
+
+const bannerList = [
+  {
+    id: '1',
+    iconName: 'icofont-duotone icofont-user-alt-4',
+    text: '1.5 Million Customers',
+  },
+  {
+    id: '2',
+    iconName: 'icofont-duotone icofont-notification',
+    text: 'More then 2000 Marchent',
+  },
+  {
+    id: '3',
+    iconName: 'icofont-duotone icofont-globe',
+    text: 'Buy Anything Online',
+  },
+];
+
+const Hero = () => {
+  return (
+    <section>
+      <h2>Hero</h2>
+    </section>
+  );
+};
+
+export default Hero;
