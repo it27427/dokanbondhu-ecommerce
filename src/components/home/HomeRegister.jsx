f103f485efabf81becfe2697eb488ed4172b5152
it27427 <istiak.tushar.qtec@gmail.com>
2024-10-21 22:24:25 +0600

    Home-Register Section Completed

--- src/components/home/HomeRegister.jsx
+++ src/components/home/HomeRegister.jsx
@@ -1,10 +1,64 @@
 import { useState } from 'react';
 
+const subTitle = 'Save The Day';
+const title = (
+  <h2 className='title'>
+    Join on Day Long Free Workshop for{' '}
+    <strong>
+      Advance <span>Mastering</span>
+    </strong>{' '}
+    on Sales
+  </h2>
+);
+const description = 'Limited Time Offer';
+
 const HomeRegister = () => {
   return (
-    <div>
-      <h2>Home Register</h2>
-    </div>
+    <section className='register-section padding-tb pb-0'>
+      <div className='container'>
+        <div className='row g-4 row-cols-lg-2 row-cols-1 align-items-center'>
+          <div className='col'>
+            <div className='section-header'>
+              <h4 className='subtitle'>{subTitle}</h4>
+
+              {title}
+
+              <p>{description}</p>
+            </div>
+          </div>
+
+          <div className='col'>
+            <div className='section-wrapper'>
+              <h4>Register Now</h4>
+              <form className='register-form'>
+                <input
+                  type='text'
+                  name='name'
+                  placeholder='Username'
+                  className='reg-input'
+                />
+                <input
+                  type='email'
+                  name='email'
+                  placeholder='E-mail'
+                  className='reg-input'
+                />
+                <input
+                  type='tel'
+                  name='phone'
+                  placeholder='Phone'
+                  className='reg-input'
+                />
+
+                <button type='submit' className='lab-btn mt-2'>
+                  Register Now
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
