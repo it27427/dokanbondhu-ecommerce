4e227c8f7f80eabca9a15cc2b731fb7a40a46d38
it27427 <istiak.tushar.qtec@gmail.com>
2024-10-21 23:28:30 +0600

    HomeAbout Data Added

--- src/components/home/HomeAbout.jsx
+++ src/components/home/HomeAbout.jsx
@@ -1,3 +1,27 @@
+const subTitle = 'Why Choose Us';
+const title = 'Become a Marchant';
+const desc =
+  'Take courses on your any device with our app & learn all about business what you want. Just download & install & start to learn';
+const btnText = 'Apply Now';
+
+const countList = [
+  {
+    iconName: 'icofont-duotone icofont-users-alt-4',
+    count: '12600',
+    text: 'Marchant Enrolled',
+  },
+  {
+    iconName: 'icofont-duotone icofont-graduate-alt',
+    count: '30',
+    text: 'Certified Courses',
+  },
+  {
+    iconName: 'icofont-duotone icofont-notification',
+    count: '100',
+    text: 'Rewards and GitCards',
+  },
+];
+
 const HomeAbout = () => {
   return (
     <section className=''>
