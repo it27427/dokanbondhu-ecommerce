83bbfe9fe527aa80bbf6901a6b88884c8ef6287c
it27427 <istiak.tushar.qtec@gmail.com>
2024-10-22 00:22:16 +0600

    app-section btn style updated

--- src/components/home/AppSection.jsx
+++ src/components/home/AppSection.jsx
@@ -1,8 +1,22 @@
+import { Link } from 'react-router-dom';
+
+const btnText = 'Sign up for Free';
+const title = 'Shop Anytime, Anywhere';
+const descriptions =
+  'Take shop on your any device with our app & learn all time what you want. Just download and install & start to learn.';
+
 const AppSection = () => {
   return (
-    <section className=''>
+    <section className='app-section padding-tb'>
       <div className='container'>
-        <h2>App Section</h2>
+        {/* SECTION-HEADER */}
+        <div className='section-header text-center'>
+          <Link to={'/sign-up'} className='lab-btn mb-4'>
+            {btnText}
+          </Link>
+          <h2 className='title'>{title}</h2>
+          <p>{descriptions}</p>
+        </div>
       </div>
     </section>
   );
