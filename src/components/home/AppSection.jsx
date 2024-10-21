bf7268dae0e5c0085cc5ef84bcd5211d3c4599f4
it27427 <istiak.tushar.qtec@gmail.com>
2024-10-22 00:00:57 +0600

    AppSection Component Created

---
+++ src/components/home/AppSection.jsx
@@ -0,0 +1,11 @@
+const AppSection = () => {
+  return (
+    <section className=''>
+      <div className='container'>
+        <h2>App Section</h2>
+      </div>
+    </section>
+  );
+};
+
+export default AppSection;
