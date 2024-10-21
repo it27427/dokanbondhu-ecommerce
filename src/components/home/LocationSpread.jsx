5591926aeb6d538a9dac965c9644ebd59ea389b2
it27427 <istiak.tushar.qtec@gmail.com>
2024-10-21 22:35:34 +0600

    LocationSpread Data Added

--- src/components/home/LocationSpread.jsx
+++ src/components/home/LocationSpread.jsx
@@ -1,4 +1,45 @@
-import React from 'react';
+const title = 'More Then 60,000 Customers';
+
+const desc =
+  'Buy products on your any device with our app & enjoy your time what you want. Just download & install & start to shopping';
+
+const clientsList = [
+  {
+    imgUrl: '/src/assets/images/clients/avater.jpg',
+    imgAlt: 'education-thumb-1',
+    text: 'Join with Us',
+  },
+  {
+    imgUrl: '/src/assets/images/clients/avater.jpg',
+    imgAlt: 'education-thumb-2',
+    text: 'Join with Us',
+  },
+  {
+    imgUrl: '/src/assets/images/clients/avater.jpg',
+    imgAlt: 'education-thumb-3',
+    text: 'Join with Us',
+  },
+  {
+    imgUrl: '/src/assets/images/clients/avater.jpg',
+    imgAlt: 'education-thumb-4',
+    text: 'Join with Us',
+  },
+  {
+    imgUrl: '/src/assets/images/clients/avater.jpg',
+    imgAlt: 'education-thumb-5',
+    text: 'Join with Us',
+  },
+  {
+    imgUrl: '/src/assets/images/clients/avater.jpg',
+    imgAlt: 'education-thumb-6',
+    text: 'Join with Us',
+  },
+  {
+    imgUrl: '/src/assets/images/clients/avater.jpg',
+    imgAlt: 'education-thumb-7',
+    text: 'Join with Us',
+  },
+];
 
 const LocationSpread = () => {
   return (
