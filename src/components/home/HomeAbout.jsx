28446a10de9dec6bbc1d14636a72790f5ca91ee8
it27427 <istiak.tushar.qtec@gmail.com>
2024-10-21 22:47:32 +0600

    LocationSpread Completed and HomeAbout Created

---
+++ src/components/home/HomeAbout.jsx
@@ -0,0 +1,11 @@
+const HomeAbout = () => {
+  return (
+    <section className=''>
+      <div className='container'>
+        <h2>Home About</h2>
+      </div>
+    </section>
+  );
+};
+
+export default HomeAbout;
--- src/components/home/LocationSpread.jsx
+++ src/components/home/LocationSpread.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 const title = 'More Then 60,000 Customers';
 
 const desc =
@@ -5,36 +7,43 @@ const desc =
 
 const clientsList = [
   {
+    id: 1,
     imgUrl: '/src/assets/images/clients/avater.jpg',
     imgAlt: 'education-thumb-1',
     text: 'Join with Us',
   },
   {
+    id: 2,
     imgUrl: '/src/assets/images/clients/avater.jpg',
     imgAlt: 'education-thumb-2',
     text: 'Join with Us',
   },
   {
+    id: 3,
     imgUrl: '/src/assets/images/clients/avater.jpg',
     imgAlt: 'education-thumb-3',
     text: 'Join with Us',
   },
   {
+    id: 4,
     imgUrl: '/src/assets/images/clients/avater.jpg',
     imgAlt: 'education-thumb-4',
     text: 'Join with Us',
   },
   {
+    id: 5,
     imgUrl: '/src/assets/images/clients/avater.jpg',
     imgAlt: 'education-thumb-5',
     text: 'Join with Us',
   },
   {
+    id: 6,
     imgUrl: '/src/assets/images/clients/avater.jpg',
     imgAlt: 'education-thumb-6',
     text: 'Join with Us',
   },
   {
+    id: 7,
     imgUrl: '/src/assets/images/clients/avater.jpg',
     imgAlt: 'education-thumb-7',
     text: 'Join with Us',
@@ -43,9 +52,29 @@ const clientsList = [
 
 const LocationSpread = () => {
   return (
-    <section className=''>
+    <section className='clients-section style-2 padding-tb'>
       <div className='container'>
-        <h2>Location</h2>
+        {/* SECTION-HEADER */}
+        <div className='section-header text-center'>
+          <h2 className='title'>{title}</h2>
+          <p>{desc}</p>
+        </div>
+
+        {/* MAIN-CONTENT */}
+        <div className='section-wrapper'>
+          <div className='clients'>
+            {clientsList.map((client) => (
+              <div className='client-list' key={client.id}>
+                <Link to='sign-up' className='client-content'>
+                  <span>{client.text}</span>
+                </Link>
+                <figure className='client-thumb'>
+                  <img src={client.imgUrl} alt={client.imgUrl} />
+                </figure>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </section>
   );
