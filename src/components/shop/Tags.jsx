900d1317fd9dd52553b8a003a227dcec6889210e
it27427 <istiak.tushar.qtec@gmail.com>
2024-10-22 21:18:29 +0600

    Tags Component Created

---
+++ src/components/shop/Tags.jsx
@@ -0,0 +1,55 @@
+const title = 'Our Popular Tags';
+
+const tagsList = [
+  {
+    id: 1,
+    href: '#',
+    text: 'envato',
+  },
+  {
+    id: 2,
+    href: '#',
+    text: 'themeforest',
+  },
+  {
+    id: 3,
+    href: '#',
+    text: 'codecanyon',
+  },
+  {
+    id: 4,
+    href: '#',
+    text: 'videohive',
+  },
+  {
+    id: 5,
+    href: '#',
+    text: 'audiojungle',
+  },
+  {
+    id: 6,
+    href: '#',
+    text: '3docean',
+  },
+  {
+    id: 7,
+    href: '#',
+    text: 'envato',
+  },
+  {
+    id: 8,
+    href: '#',
+    text: 'themeforest',
+  },
+  {
+    id: 9,
+    href: '#',
+    text: 'codecanyon',
+  },
+];
+
+const Tags = () => {
+  return <div>Tags</div>;
+};
+
+export default Tags;
