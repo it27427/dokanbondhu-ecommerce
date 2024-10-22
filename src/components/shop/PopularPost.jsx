9daa372a674bca1b81e3bb55ec80300a2ce598dc
it27427 <istiak.tushar.qtec@gmail.com>
2024-10-22 20:30:16 +0600

    PopularPost Component Created

---
+++ src/components/shop/PopularPost.jsx
@@ -0,0 +1,40 @@
+const postList = [
+  {
+    id: 1,
+    imgUrl: '/src/assets/images/blog/10.jpg',
+    imgAlt: 'rajibraj91',
+    title: 'Poor People Campaign Our Resources',
+    date: 'Jun 05,2022',
+  },
+  {
+    id: 2,
+    imgUrl: '/src/assets/images/blog/11.jpg',
+    imgAlt: 'rajibraj91',
+    title: 'Poor Peoples Campaign Our Resources',
+    date: 'Jun 05,2022',
+  },
+  {
+    id: 3,
+    imgUrl: '/src/assets/images/blog/12.jpg',
+    imgAlt: 'rajibraj91',
+    title: 'Poor Peoples Campaign Our Resources',
+    date: 'Jun 05,2022',
+  },
+  {
+    id: 4,
+    imgUrl: '/src/assets/images/blog/09.jpg',
+    imgAlt: 'rajibraj91',
+    title: 'Poor Peoples Campaign Our Resources',
+    date: 'Jun 05,2022',
+  },
+];
+
+const PopularPost = () => {
+  return (
+    <div>
+      <h2>Popular Post</h2>
+    </div>
+  );
+};
+
+export default PopularPost;
