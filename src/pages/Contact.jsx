fd249fe17194f02805b8ff3a90c163d380b1ff65
it27427 <istiak.tushar.qtec@gmail.com>
2024-10-22 15:34:42 +0600

    Contact Page Created

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <main className='main-section'>
+      <section>
+        <div className='container'>
+          <h2>Contact Page</h2>
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default Contact;
