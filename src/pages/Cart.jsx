85cd80ffc37db5f5d63cf9efd1b3769e3b3dae81
it27427 <istiak.tushar.qtec@gmail.com>
2024-10-23 14:19:16 +0600

    cart-top-part done

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,13 +1,137 @@
+import { useState, useEffect } from 'react';
 import BannerSection from '@/components/global/BannerSection';
+import { Link } from 'react-router-dom';
 
 const Cart = () => {
+  const [cartItems, setCartItems] = useState([]);
+
+  useEffect(() => {
+    // FETCH CART-ITEMS FROM LOCAL STORAGE
+    const storedCartItems = JSON.parse(localStorage.getItem('cart')) || [];
+    setCartItems(storedCartItems);
+  }, []);
+
+  // CALCULATE PRICES
+  const calculateTotalPrice = (item) => {
+    return item.price * item.quantity;
+  }
+
+  // HANDLE QUANTITY INCRESE
+  const handleIncrese = (item) => {
+    item.quantity += 1;
+    setCartItems([...cartItems]);
+
+    // UPDATE LOCAL-STORAGE WITH NEW CART ITEMS
+    localStorage.setItem('cart', JSON.stringify(cartItems));
+  }
+
+  // HANDLE QUANTITY DECRESE
+  const handleDecrese = (item) => {
+    if(item.quantity > 1) {
+      item.quantity -= 1;
+      setCartItems([...cartItems]);
+    
+      // UPDATE LOCAL-STORAGE WITH NEW CART ITEMS
+      localStorage.setItem('cart', JSON.stringify(cartItems));
+    }
+  }
+
+  // HANDLE REMOVE ITEM
+  const handleRemoveItem = (item) => {
+    const updatedCart = cartItems.filter((cartItem) => cartItem.id !== item.id);
+    
+    // UPDATE NEW CART
+    setCartItems(updatedCart);
+    updateLocalStorage(updatedCart);
+  }
+
+  const updateLocalStorage = (cart) => {
+    localStorage.setItem('cart', JSON.stringify(cart));
+  }
+
+  const cartSubTotal = cartItems.reduce((total, item) => {
+    return total + calculateTotalPrice(item);
+  }, 0);
+
+  const orderTotal = cartSubTotal;
+
   return (
     <main className='main-section'>
       <BannerSection title='Shopping Cart' currentPage='Cart' />
 
-      <section>
+      <section className='shop-cart padding-tb'>
         <div className='container'>
-          <h2>Cart</h2>
+          <div className="section-wrapper">
+            <div className="cart-top">
+              <table>
+                <thead>
+                  <tr>
+                    <th className='cat-product'>Product</th>
+                    <th className='cat-price'>Price</th>
+                    <th className='cat-quantity'>Quantity</th>
+                    <th className='cat-toprice'>Total</th>
+                    <th className='cat-edit'>Edit</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {
+                    cartItems.map((item, i) => (
+                      <tr key={i}>
+                        <td className='product-item cat-product'>
+                          <figure className="p-thumb">
+                            <Link to='/shop'>
+                              <img src={item.img} alt={item.name} />
+                            </Link>
+                          </figure>
+                          <div className="p-content">
+                            <Link to='/shop'>{item.name}</Link>
+                          </div>
+                        </td>
+
+                        <td className="cat-price">$ {item.price}</td>
+
+                        <td className="cat-quantity">
+                          <div className='cart-plus-minus'>
+                            <button
+                              type='button'
+                              onClick={() => handleDecrese(item)}
+                              className='dec qtybutton d-flex align-items-center justify-content-center'
+                            >
+                              <i className='icofont-minus'></i>
+                            </button>
+                            <input
+                              type='text'
+                              name='qtybutton'
+                              id='qtybutton'
+                              placeholder='0'
+                              className='cart-plus-minus-box'
+                              value={item.quantity}
+                              onChange={(e) => setQuantity(parseInt(e.target.value, 10))}
+                            />
+                            <button
+                              type='button'
+                              onClick={() => handleIncrese(item)}
+                              className='inc qtybutton d-flex align-items-center justify-content-center'
+                            >
+                              <i className='icofont-plus'></i>
+                            </button>
+                          </div>
+                        </td>
+
+                        <td className='cat-toprice'>${calculateTotalPrice(item)}</td>
+
+                        <td className='cat-edit'>
+                          <button type='button' className='bg-transparent text-warning' onClick={() => handleRemoveItem(item)}>
+                            <i className="icofont-ui-delete"></i>
+                          </button>
+                        </td>
+                      </tr>
+                    ))
+                  }
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
       </section>
     </main>
