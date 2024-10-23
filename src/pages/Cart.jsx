2271b26b21b7ee434462fd7df36035d3605babd3
it27427 <istiak.tushar.qtec@gmail.com>
2024-10-23 13:18:16 +0600

    Cart Page BannerSection Added

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,6 +1,10 @@
+import BannerSection from '@/components/global/BannerSection';
+
 const Cart = () => {
   return (
     <main className='main-section'>
+      <BannerSection title='Shopping Cart' currentPage='Cart' />
+
       <section>
         <div className='container'>
           <h2>Cart</h2>
